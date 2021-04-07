6b413885937e3c4a6b9930ce5a98a5259573c052
sim-woojeong <dnwjd040635@gmail.com>
2021-04-07 11:08:54 +0900

    get sensor list using ScrollView + button

--- app/src/main/java/com/example/getsensorlist/MainActivity.java
+++ app/src/main/java/com/example/getsensorlist/MainActivity.java
@@ -6,6 +6,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mTxtSensors;
     SensorManager sensorMgr;
     List<Sensor> sensorList;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTxtSensors = findViewById(R.id.txtSensors);
-        mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
+    //    mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
 
-        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mButton = findViewById(R.id.button);
 
-        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+                sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
 
-        mTxtSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");
-        for(Sensor sensor : sensorList){ // Enhanced for Loop
-            mTxtSensors.append("Sensor name: " + sensor.getName() + "\n");
-            mTxtSensors.append("Sensor type: " + sensor.getType() + "\n\n");
-        }
+                mTxtSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");
+                for(Sensor sensor : sensorList){ // Enhanced for Loop
+                    mTxtSensors.append("Sensor name: " + sensor.getName() + "\n");
+                    mTxtSensors.append("Sensor type: " + sensor.getType() + "\n\n");
+                }
+            }
+        });
 
     }
 }
